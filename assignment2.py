d55a72f7901a965579dd6802dd9295d509643768
simrantulseja <simran1858.cse16@chitkara.edu.in>
2018-08-17 15:16:25 +0530

    this is my second assignment

---
+++ assignment2.py
@@ -0,0 +1,20 @@
+print("my name is simran tulseja")
+a='Acad'
+b='view'
+print("Acad + View =")
+print(a+b)
+x=input("enter firt value")
+y=input("enter second value")
+z=input("enter second value")
+print("x=" ,x)
+print("y=" ,y)
+print("z=" ,z)
+print("Let's get started")
+s="Acadview"
+course ="python"
+fees=5000
+print("%s %s %d" %(s,course,fees))
+name = "Tony Stark"
+salary = 1000000
+print("%s %d"%(name,salary))
+
