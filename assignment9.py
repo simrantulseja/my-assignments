2dcc162c3864c6b2e709fdf1f108805049c8efb7
simrantulseja <42332438+simrantulseja@users.noreply.github.com>
2018-09-03 14:09:24 +0530

    Add files via upload

---
+++ assignment9.py
@@ -0,0 +1,5 @@
+#Q.1- Write a python code to find a valid email address from a text.
+#Ans:[a-zA-Z0-9_.]*[@](gmail.com|yahoo.com|icloud.com|hotmail.com|rediffmail.com)
+
+#Q.2- Write a python program to find a valid Indian phone number from a text.(Valid Indian numbers will start with "+91-" and after that [6-9] followed by 9 digits.)
+#Ans:^(+91-)[6-9]\d{9}
